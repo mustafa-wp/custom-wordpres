73ddb4dbc65b990c50398cd9cfd65b6b8c260003
mustafamahmou23 <mustafamahmou23@gmail.com>
2025-05-04 19:07:57 +0300

    custom-wordpres1

--- functions.php
+++ functions.php
@@ -38,23 +38,4 @@ function custom_features()
 add_action('after_setup_theme', 'custom_features');
 
 // دالة لتسجيل نوع منشور مخصص: الفعاليات (Events)
-function custom_post_types()
-{
-    register_post_type('event', array(
-        'public' => true,
-        'has_archive' => true,
-        'rewrite' => array('slug' => 'events'),
-        'supports' => array('title', 'editor', 'excerpt', 'thumbnail'),
-        'labels' => array(
-            'name' => 'Events',
-            'singular_name' => 'Event',
-            'add_new_item' => 'Add New Event',
-            'edit_item' => 'Edit Event',
-            'new_item' => 'New Event',
-            'view_item' => 'View Event',
-            'search_items' => 'Search Events'
-        ),
-        'menu_icon' => 'dashicons-calendar-alt'
-    ));
-}
-add_action('init', 'custom_post_types');
+
